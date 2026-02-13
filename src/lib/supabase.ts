66e6b2fd138ae0fa3cbba7342dab0ee851287d8b
Hal Chang <jcgk2ndbjy@gmail.com>
2026-02-14 04:35:30 +0800

    refactor: 將金鑰移至環境變數

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // 這些金鑰可以在 Supabase 的 Project Settings > API 找到
-const supabaseUrl = "https://ecncnhcoxytzbgarurvn.supabase.co";
-const supabaseAnonKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjbmNuaGNveHl0emJnYXJ1cnZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5OTI5NDMsImV4cCI6MjA4NjU2ODk0M30.9cpOBZGANNCaAAEV2wiUIceKB66ixYjsrypOROlTtWI";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
